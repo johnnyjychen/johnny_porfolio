2c7c7946829118c4d51bb0a2c98493943a216152
johnnyjiongyu <jiongyu@umich.edu>
2024-02-28 13:22:41 -0500

    Update js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // mobile device hamburger button
     const menuButton = document.querySelector('.hamburger_menu');
     const navList = document.querySelector('.nav_list'); 
     menuButton.addEventListener('click', function(screen) {
@@ -14,4 +15,55 @@ document.addEventListener('DOMContentLoaded', function() {
         if (!navList.contains(screen.target) && screen.target !== menuButton) {
             navList.style.right = '-800px';}
     });
+
+    // img hover
+    document.querySelectorAll('.nav_link').forEach(link => {
+        const home = link.querySelector('.home_hover');
+        link.addEventListener('mouseover', function() {
+            const hoverSrc = home.getAttribute('data-hover');
+            if (hoverSrc) home.src = hoverSrc;
+        });
+        link.addEventListener('mouseout', function() {
+            const originalSrc = home.getAttribute('data-original');
+            if (originalSrc) home.src = originalSrc;
+        });
+    });
+
+    document.querySelectorAll('.nav_link').forEach(link => {
+        const work = link.querySelector('.work_hover');
+        link.addEventListener('mouseover', function() {
+            const hoverSrc = work.getAttribute('data-hover');
+            if (hoverSrc) work.src = hoverSrc;
+        });
+        link.addEventListener('mouseout', function() {
+            const originalSrc = work.getAttribute('data-original');
+            if (originalSrc) work.src = originalSrc;
+        });
+    });
+
+    document.querySelectorAll('.nav_link').forEach(link => {
+        const about = link.querySelector('.about_hover');
+        link.addEventListener('mouseover', function() {
+            const hoverSrc = about.getAttribute('data-hover');
+            if (hoverSrc) about.src = hoverSrc;
+        });
+        link.addEventListener('mouseout', function() {
+            const originalSrc = about.getAttribute('data-original');
+            if (originalSrc) about.src = originalSrc;
+        });
+    });
+
+    document.querySelector('.linkedin_icon').addEventListener('mouseover', function(){
+        this.src = './images/linkedin_highlight.png'
+    })
+    document.querySelector('.linkedin_icon').addEventListener('mouseout', function(){
+        this.src = './images/linkedin.png'
+    })
+
+    document.querySelector('.email_icon').addEventListener('mouseover', function(){
+        this.src = './images/email_highlight.png'
+    })
+    document.querySelector('.email_icon').addEventListener('mouseout', function(){
+        this.src = './images/email.png'
+    })
 });
